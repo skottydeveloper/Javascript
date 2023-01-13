c93a71651a00737a59686e30da10120ea30b2c0c
Andreas Skotadis <skottydeveloper@gmail.com>
2023-01-14 08:50:57 +1100

    Complete second challenge.

--- projects/freecodecamp_mongodb_and_mongoose_challenges/boilerplate-mongomongoose/myApp.js
+++ projects/freecodecamp_mongodb_and_mongoose_challenges/boilerplate-mongomongoose/myApp.js
@@ -4,7 +4,16 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
-let Person;
+/* 2. Create a Model */
+const Schema = mongoose.Schema;
+
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
+
+const Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
