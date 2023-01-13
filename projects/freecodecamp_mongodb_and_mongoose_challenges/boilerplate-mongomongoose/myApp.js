ae2498a8e67b1cab249841ffac043112eaf20065
Andreas Skotadis <skottydeveloper@gmail.com>
2023-01-13 20:57:24 +1100

    Complete first challenge.

--- projects/freecodecamp_mongodb_and_mongoose_challenges/boilerplate-mongomongoose/myApp.js
+++ projects/freecodecamp_mongodb_and_mongoose_challenges/boilerplate-mongomongoose/myApp.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 
+/* 1. Install & Setup Mongoose */
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 let Person;
 
