25602a9e45121027f378bcce95d653555ecc581a
Andreas Skotadis <skottydeveloper@gmail.com>
2023-01-14 11:41:46 +1100

    Fix npm start scripts not found problem + implement initial code for the app without the CSS.

--- projects/random_quote_machine/random-quote-machine/src/App.js
+++ projects/random_quote_machine/random-quote-machine/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [quotes, setQuotes] = useState("");
+
+  const getQuote = () => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((data) => {
+        let randomNum = Math.floor(Math.random() * data.length);
+        setQuotes(data[randomNum]);
+      });
+  };
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="quote">
+        <p>{quotes.text}</p>
+        <p>{quotes.author}</p>
+        <div className="btnContainer">
+          <button className="btn">Get Quote</button>
+          <a href="" className="btn">Tweet</a>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
