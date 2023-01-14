f9fb2b15434e5eb48a5fd10ba39787b3e879ffb5
Andreas Skotadis <skottydeveloper@gmail.com>
2023-01-14 12:31:46 +1100

    Fix code according to use cases provided.

--- projects/random_quote_machine/random-quote-machine/src/App.js
+++ projects/random_quote_machine/random-quote-machine/src/App.js
@@ -19,16 +19,16 @@ const App = () => {
 
   return (
     <div className="App">
-      <div className="quote">
-        <p>{quotes.text}</p>
-        <p>- {quotes.author}</p>
+      <div className="quote-box">
+        <p id="text">"{quotes.text}"</p>
+        <p id="author">- {quotes.author}</p>
         <div className="btnContainer">
-          <button onClick ={getQuote} className="btn">Get Quote</button>
+          <button onClick ={getQuote} className="new-quote">Get Quote</button>
           <a 
             href={`https://twitter.com/intent/tweet?text=${quotes.text}`} 
             target="_blank"
             rel="noopener noreferrer"
-            className="btn">Tweet</a>
+            className="tweet-quote">Tweet</a>
         </div>
       </div>
     </div>
