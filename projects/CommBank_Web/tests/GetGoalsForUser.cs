1208b74a1cf6f81d94ecb6c3cc00f645999aa4b0
Andreas Skotadis <skottydeveloper@gmail.com>
2023-02-19 17:03:30 +1100

    Cover GetGoalsForUser route.

---
+++ projects/CommBank_Web/tests/GetGoalsForUser.cs
@@ -0,0 +1,33 @@
+public class GoalControllerTests {
+    private readonly FakeCollections collections;
+
+    public GoalControllerTests() {
+        collections = new();
+    }
+
+    [Fact]
+    public async void GetForUser() {
+        var goals = collections.GetGoals();
+        var users = collections.GetUsers();
+        
+        IGoalsService goalsService = new FakeGoalsService(goals, goals[0]);
+        IUsersService usersService = new FakeUsersService(users, users[0]);
+        GoalController controller = new(goalsService, usersService);
+
+        var httpContext = new Microsoft.AspNetCore.Http.DefaultHttpContext();
+        
+        controller.ControllerContext.HttpContext = httpContext;
+        
+        var result = await controller.GetForUser(goals[0].UserId!);
+
+        Assert.NotNull(result);
+
+        var index = 0;
+        
+        foreach (Goal goal in result!) {
+            Assert.IsAssignableFrom<Goal>(goal);
+            Assert.Equal(goals[0].UserId, goal.UserId);
+            index++;
+        }
+    }
+}
