e00298fc083cff054af600f0db99a63718eab6c5
Andreas Skotadis <skottydeveloper@gmail.com>
2023-04-10 00:13:46 +1000

    Add conditional checks.

--- projects/contact_manager/contact-app/src/components/Contacts.js
+++ projects/contact_manager/contact-app/src/components/Contacts.js
@@ -1,42 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Profile from './Profile';
 import error from '../images/question-mark.png';
 
 const Contacts = ({ contacts }) => {
-    return (
-        <div>
-            {
-                contacts.length
-                    ?
-                    <div className="text-start mt-0 mt-md-4">
-                        <table className="table table-hover">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Name</th>
-                                    <th className="d-none d-lg-table-cell" scope="col">Phone</th>
-                                    <th className="d-none d-lg-table-cell" scope="col">Email</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    contacts.map(user => <Profile
-                                        key={user.id}
-                                        user={user}
-                                    ></Profile>)
-                                }
-                            </tbody>
-                        </table>
+  const [isLoading, setIsLoading] = useState(true);
 
-                    </div>
-                    :
-                    <div className="mt-4">
-                        <h1 className="fw-bold">No Contacts Found</h1>
-                        <p>No results retrieved.</p>
-                        <img className="img-fluid col-9 col-md-6 col-lg-3 mx-auto" src={error} alt="" />
-                    </div>
-            }
+  useEffect(() => {
+    if (contacts) {
+      setIsLoading(false);
+    }
+  }, [contacts]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!Array.isArray(contacts)) {
+    return null;
+  }
+
+  return (
+    <div>
+      {contacts.length ? (
+        <div className="text-start mt-0 mt-md-4">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th className="d-none d-lg-table-cell" scope="col">Phone</th>
+                <th className="d-none d-lg-table-cell" scope="col">Email</th>
+              </tr>
+            </thead>
+            <tbody>
+              {contacts.map((user) => (
+                <Profile key={user.id} user={user}></Profile>
+              ))}
+            </tbody>
+          </table>
         </div>
-    );
+      ) : (
+        <div className="mt-4">
+          <h1 className="fw-bold">No Contacts Found</h1>
+          <p>No results retrieved.</p>
+          <img
+            className="img-fluid col-9 col-md-6 col-lg-3 mx-auto"
+            src={error}
+            alt=""
+          />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Contacts;
